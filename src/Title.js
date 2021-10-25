7c2f82630cc4ccedc06b0e4783cdef98ee47692e
DanVlf <daniel.volf01@gmail.com>
2021-10-25 15:44:26 +0200

    commit on 'master'

---
+++ src/Title.js
@@ -0,0 +1,17 @@
+// src/Title.js
+
+
+
+import React from 'react'
+import './Title.css';
+
+function Title() {
+  return (
+    <div className="Title">
+      <h1>Daniel Volf</h1>
+            <div className="Title-Subtitle"></div>
+    </div>
+  )
+}
+
+export default Title
